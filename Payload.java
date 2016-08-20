220c74068b9d4bbf0ef0de0476fcc68c35333386
Francisco Salgado <salgfrancisco@gmail.com>
2016-08-21 00:40:11 +0100

    Solution

---
+++ Payload.java
@@ -0,0 +1,11 @@
+public class Payload {
+
+    public static void main(String[] args) {
+        Integer localInteger = Integer.valueOf(656);
+	System.out.println(Integer.valueOf(686964656).toString() + "c" + Integer.valueOf(696).toString() + "b" + Integer.valueOf(656163).toString() + Integer.valueOf(68616).toString() + "d" + localInteger.toString() + "c" + localInteger.toString() + "f6e");
+	localInteger = Integer.valueOf(696);
+	System.out.println(Integer.valueOf(6265).toString() + Integer.valueOf(66696).toString() + Integer.valueOf(57263656).toString() + "c" + localInteger.toString() + "b" + Integer.valueOf(65616).toString() + "c" + localInteger.toString() + "f6e");}
+}
+
+
+
